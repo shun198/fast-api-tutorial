8c81e39697bfc0826441f3acf7e55cebcad2e418
shun198 <shunhiroseluvmri@gmail.com>
2022-07-17 11:02:57 +0900

    :+1: use enums for HTTP GET in models

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from enum import Enum
+from math import gamma
+
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alpha = "alpha"
+    beta = "beta"
+    gamma = "gamma"
+
 
 app = FastAPI()
 
@@ -16,3 +26,17 @@ async def read_item(item_id: int):
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alpha:
+        return {"model_name": model_name, "message": "Alpha is called"}
+
+    if model_name.value == ModelName.beta:
+        return {"model_name": model_name, "message": "Beta is called"}
+
+    if model_name.value == ModelName.gamma:
+        return {"model_name": model_name, "message": "Gamma is called"}
+
+    return {"model_name": model_name, "message": "Test model message"}
