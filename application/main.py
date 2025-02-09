7e2ba47a7b102a706c27600d8568d2a60c7b7330
shun198 <shunhiroseluvmri@gmail.com>
2025-02-09 10:57:25 +0900

    👍 change to read_todos

--- application/main.py
+++ application/main.py
@@ -27,7 +27,7 @@ def health_check():
 # https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-with-yield/?h=get_db#sub-dependencies-with-yield
 # https://github.com/fastapi/fastapi/pull/9298
 # FastAPI0.95.0以降の機能
-@app.get("/api/books")
+@app.get("/api/todos")
 # 依存性注入をすることでget_dbメソッドが自動的に呼ばるので毎回セッションインスタンスの生成やセッションを切る処理を書かずに済む
 # Annotatedを使うことでDepends(get_db)がSession型だとわかる
 # https://fastapi.tiangolo.com/tutorial/sql-databases/#create-a-session-dependency
