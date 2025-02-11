6155c19bbfbb7dd7563b9b0f8105ab2479ab3127
shun198 <shunhiroseluvmri@gmail.com>
2025-02-11 13:41:23 +0900

    👍 delete unnecessary comments

--- application/main.py
+++ application/main.py
@@ -1,14 +1,8 @@
-# from database import engine
 from fastapi import FastAPI
-
-# from models import Base
 from routers import auth, todos
 
 app = FastAPI()
 
-# https://docs.sqlalchemy.org/en/20/core/metadata.html#sqlalchemy.schema.MetaData.create_all
-# テーブルを作成
-# Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 
